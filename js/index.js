6022482c1ace5ac3670a902f6e261bcbad1cdbcc
micky rendon <elderendon@hotmail.es>
2022-10-20 22:25:00 -0300

    features

--- js/index.js
+++ js/index.js
@@ -1,22 +1,105 @@
-// - REPLICA EL EJERCICIO COMPLETO DEL VÍDEO
+window.onload = emptyList
+// empty list
+function emptyList(){
+    const ol =  document.querySelector('.list')
+    
+    if(ol.childElementCount === 0){
+        console.log('if');
+        const mesg = document.createElement('span')
+        mesg.classList.add('mesg')
+        mesg.style.display = 'inline-block'
+        const txt = document.createTextNode('Lista vacía, agrega un nuevo ítem')
+
+        ol.appendChild(mesg)
+        mesg.appendChild(txt)
+    }else{
+        console.log('cartel eliminado');
+// todo: ocultar el cartel creado
+        // mesg.style.display = 'none'
+
+    }
+    console.log(ol.childElementCount);
+
+}
+// form add item
+function prevent(){
+    const addF = document.forms['form-add']
+
+    addF.addEventListener('submit', e => {
+        console.log('prevenido');
+        e.preventDefault()
+    })
+}
+
+document.querySelector('.btn').addEventListener('click', () => {
+    prevent()
+    newItem()
+})
+
+// new item
+function newItem(){
+    // inputs value
+    const name =   document.querySelector('.name')
+    const price =  document.querySelector('.price')
+    // parentnode
+    const ol =  document.querySelector('.list')
+    //new html element
+    const li =  document.createElement('li')  
+    li.classList.add('paragraph', 'item')
+    li.setAttribute('draggable', 'true')
+    // todo: sett id 
+    li.setAttribute('id', 'p-5')
+    // li child
+    const ctr       = document.createElement('div')  
+    ctr.classList.add('ctr')
+    const spanName  = document.createElement('span')  
+    const spanPrice = document.createElement('span')  
+    // adding text content
+    const liVal = document.createTextNode(name.value)
+    const priceVal = document.createTextNode(price.value)
+
+    ol.appendChild(li)
+    li.appendChild(ctr)
+    ctr.appendChild(spanName)
+    ctr.appendChild(spanPrice)
+    spanName.appendChild(liVal)
+    spanPrice.appendChild(priceVal)
+}
+
+// paperBin
+const paperBin = document.querySelector(".paper-bin")
+
+paperBin.addEventListener("dragover", e => {
+    e.preventDefault()
+    e.dataTransfer.dropEffect = "copy"
+})
+
+paperBin.addEventListener("drop", e => {
+    const idPrg = e.dataTransfer.getData("id")
+    const removeP = document.getElementById(idPrg)
+    removeP.remove()
+})
+
+// drag & drop
 const section = document.querySelectorAll('.section')
-const paragraph = document.querySelectorAll('.paragraph')
+const item = document.querySelectorAll('.item')
 
-paragraph.forEach(pgr=>{
+item.forEach( itm =>{
 
-    pgr.addEventListener('dragstart', e =>{
-        console.log(`dragging box number ${pgr.innerText}`);
+    itm.addEventListener('dragstart', e =>{
+        console.log(`dragging box number ${itm.innerText}`);
         // adding class
-        pgr.classList.add('dragging')
+        itm.classList.add('dragging')
         // it returns id because making reference to html id element
-        e.dataTransfer.setData('id', pgr.id)
+        // todo: hacer referencia a la funcion de id no al selector id
+        e.dataTransfer.setData('id', itm.id)
         // const phantom = document.querySelector('.phantom')
         // e.dataTransfer.setDragImage(phantom, 0, 0)
     })
 
-    pgr.addEventListener('dragend', () =>{
+    itm.addEventListener('dragend', () =>{
         // console.log('dragging ended');
-        pgr.classList.remove('dragging')
+        itm.classList.remove('dragging')
     })
 })
 
@@ -36,19 +119,3 @@ section.forEach(sctn =>{
         sctn.append(prgId)
     })
 })
-
-// - AÑADE EL CÓDIGO NECESARIO PARA QUE AL ARRASTRAR UN FRAGMENTO DEL PUZZLE A LA PAPELERA, ÉSTE SE ELIMINE
-// paperBin
-const paperBin = document.querySelector(".paper-bin")
-
-paperBin.addEventListener("dragover", e => {
-    e.preventDefault()
-    e.dataTransfer.dropEffect = "copy"
-})
-
-paperBin.addEventListener("drop", e => {
-    const idPrg = e.dataTransfer.getData("id")
-    const removeP = document.getElementById(idPrg)
-    removeP.remove()
-})
-
